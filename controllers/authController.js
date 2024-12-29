f446cde3858c3838a3e5d40b3fcd48f9640cfe4f
MohamedHanifProg <signutre.31@gmail.com>
2024-12-29 23:17:43 +0200

    dealing with render problems

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('../models/user');
+const User = require('../models/User');
 
 exports.login = async (req, res) => {
   try {
