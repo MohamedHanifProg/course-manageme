68b723bba00fae27be51ed8cb401d356bf990010
MohamedHanifProg <signutre.31@gmail.com>
2025-01-03 20:36:44 +0200

    validations on the code

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 require('dotenv').config();
+const connectDB = require('./config/db'); 
 const authRoutes = require('./routes/authRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const app = express();
 app.use(express.json());
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
 app.get('/', (req, res) => {
   res.status(200).send('API is running...');
 });
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
+  await connectDB(); 
 });
